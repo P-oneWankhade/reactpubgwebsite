02ede835fe47b40e0a42bad2d730f064d9b2e060
Pawan <pawanwankhade458@gmail.com>
2020-07-23 16:17:35 +0530

    Updates

---
+++ precache-manifest.ea3f2b149c471da0f01d522698d6e292.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0514ebb7372af716eedee2afc3673173",
+    "url": "/reactpubgwebsite/index.html"
+  },
+  {
+    "revision": "11a5b629875a3a297dde",
+    "url": "/reactpubgwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "1661838b35302820c905",
+    "url": "/reactpubgwebsite/static/css/main.6b3dcc63.chunk.css"
+  },
+  {
+    "revision": "11a5b629875a3a297dde",
+    "url": "/reactpubgwebsite/static/js/2.3d5cc3ee.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactpubgwebsite/static/js/2.3d5cc3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1661838b35302820c905",
+    "url": "/reactpubgwebsite/static/js/main.46b329fe.chunk.js"
+  },
+  {
+    "revision": "fa413737a1c4343a0837",
+    "url": "/reactpubgwebsite/static/js/runtime-main.2406e3e1.js"
+  },
+  {
+    "revision": "4ff3d75aec6843cb35667841d6194c2a",
+    "url": "/reactpubgwebsite/static/media/1.4ff3d75a.jpg"
+  },
+  {
+    "revision": "238b3bc9389a2d928fa9f0750c9a3326",
+    "url": "/reactpubgwebsite/static/media/2.238b3bc9.jpeg"
+  },
+  {
+    "revision": "290919fddc9bdc0d9aec416d04f018d6",
+    "url": "/reactpubgwebsite/static/media/card1.290919fd.jpg"
+  },
+  {
+    "revision": "21ed3f2690fd3d7670f4f918bd58c9b2",
+    "url": "/reactpubgwebsite/static/media/card2.21ed3f26.jpg"
+  },
+  {
+    "revision": "2b757c1507be2e484c9b637fb58e2006",
+    "url": "/reactpubgwebsite/static/media/card3.2b757c15.jpg"
+  },
+  {
+    "revision": "d1f280b07c80e2c92482110a1960ba02",
+    "url": "/reactpubgwebsite/static/media/card4.d1f280b0.jpg"
+  },
+  {
+    "revision": "f52d4536b95029196116cb7a13b56b7c",
+    "url": "/reactpubgwebsite/static/media/card5.f52d4536.jpg"
+  },
+  {
+    "revision": "06d07b54b70beab674514332bc2fe182",
+    "url": "/reactpubgwebsite/static/media/card6.06d07b54.jpg"
+  }
+]);
